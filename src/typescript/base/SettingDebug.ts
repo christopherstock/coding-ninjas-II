b347450cfaca8418c78d8f0c3fc7e5c6dc2e6a49
Christopher Stock <christopher.stock@mayflower.de>
2025-05-05 11:49:11 +0200

    welcome panel

--- src/typescript/base/SettingDebug.ts
+++ src/typescript/base/SettingDebug.ts
@@ -4,10 +4,9 @@
 export class SettingDebug {
     public static readonly DEBUG_MODE: boolean                      = true;
 
-    public static readonly DISABLE_SOUNDS: boolean                  = (false  && SettingDebug.DEBUG_MODE);
-
-    public static readonly DISABLE_POINTER: boolean                 = (true  && SettingDebug.DEBUG_MODE);
-    public static readonly DISABLE_DARKEN_PANEL: boolean            = (true  && SettingDebug.DEBUG_MODE);
+    public static readonly DISABLE_SOUNDS: boolean                  = (true && SettingDebug.DEBUG_MODE);
+    public static readonly DISABLE_POINTER: boolean                 = (true && SettingDebug.DEBUG_MODE);
+    public static readonly DISABLE_DARKEN_PANEL: boolean            = (true && SettingDebug.DEBUG_MODE);
     public static readonly NO_FLOATING_STARTUP: boolean             = (true && SettingDebug.DEBUG_MODE);
 
     public static readonly BG_FOR_MATTER_BODY: boolean              = (false && SettingDebug.DEBUG_MODE);
