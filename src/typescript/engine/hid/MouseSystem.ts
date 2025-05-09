927fa5a81f46aca1f8dfb1894b7d3e20a3543b9f
Christopher Stock <christopher.stock@mayflower.de>
2025-05-09 15:27:29 +0200

    billboard mouse hand hover

--- src/typescript/engine/hid/MouseSystem.ts
+++ src/typescript/engine/hid/MouseSystem.ts
@@ -2,6 +2,9 @@ import { DebugLog } from '../../base/DebugLog';
 import { Main } from '../../base/Main';
 import { ImageData } from '../../data/ImageData';
 import { LevelId } from '../../game/level/Level';
+import { Decoration } from '../../game/object/deco/Decoration';
+
+/* eslint-disable max-len */
 
 /** ********************************************************************************************************************
 *   The pointer system that manages all pointer interactions.
@@ -13,68 +16,112 @@ export class MouseSystem {
     public constructor() {
         DebugLog.engine.log('Setup mouse system');
 
-        // eslint-disable-next-line max-len
-        window.addEventListener('click',   (event: Event) => { this.onClick(event as PointerEvent) });
+        window.addEventListener('click',     (event: Event) => { this.onClick(event as PointerEvent) });
+        window.addEventListener('mousemove', (event: Event) => { this.onMove(event as PointerEvent) });
     }
 
     private onClick(event: PointerEvent): void {
-        // handle clicks on billboards
-        if (Main.game.level !== null) {
-            const decos = Main.game.level.decosFg.concat(Main.game.level.decosBg);
+        if (Main.game.level === null) {
+            return;
+        }
 
-            for (const deco of decos) {
-                const img: string = deco.sprite.template.imageIds[0];
-                if (!img.includes('billboard')) {continue;}
+        // get the billboard Deco object for this level
+        const relevantBillboard: Decoration = this.getRelevantBillboard();
+        if (relevantBillboard === null) {
+            return;
+        }
+        /*
+        const decos = Main.game.level.decosFg.concat(Main.game.level.decosBg);
 
-                if (
-                    event.clientX >= deco.shape.body.bounds.min.x - Main.game.camera.getOffsetX()
+        for (const deco of decos) {
+            const img: string = deco.sprite.template.imageIds[0];
+            if (!img.includes('billboard')) {continue;}
+*/
+        if (
+            event.clientX >= relevantBillboard.shape.body.bounds.min.x - Main.game.camera.getOffsetX()
                     && event.clientX < (
-                        deco.shape.body.bounds.min.x + deco.shape.getWidth() - Main.game.camera.getOffsetX()
+                        relevantBillboard.shape.body.bounds.min.x + relevantBillboard.shape.getWidth() - Main.game.camera.getOffsetX()
                     )
-                    && event.clientY >= deco.shape.body.bounds.min.y - Main.game.camera.getOffsetY()
+                    && event.clientY >= relevantBillboard.shape.body.bounds.min.y - Main.game.camera.getOffsetY()
                     && event.clientY < (
-                        deco.shape.body.bounds.min.y + deco.shape.getHeight() - Main.game.camera.getOffsetY()
+                        relevantBillboard.shape.body.bounds.min.y + relevantBillboard.shape.getHeight() - Main.game.camera.getOffsetY()
                     )
+        ) {
+            const leftHalf = event.clientX < (
+                relevantBillboard.shape.body.bounds.min.x + relevantBillboard.shape.getWidth() / 2 - Main.game.camera.getOffsetX()
+            );
+
+            if (
+                Main.game.level.id === LevelId.LEVEL_START
+            ) {
+                window.open('https://www.christopherstock.de', '_blank');
+            }
+
+            if (
+                Main.game.level.id === LevelId.LEVEL_DOJO
+            ) {
+                if (
+                    leftHalf
                 ) {
-                    const leftHalf = event.clientX < (
-                        deco.shape.body.bounds.min.x + deco.shape.getWidth() / 2 - Main.game.camera.getOffsetX()
-                    );
-
-                    if (
-                        Main.game.level.id === LevelId.LEVEL_START
-                        && img === ImageData.BILLBOARD_WELCOME
-                    ) {
-                        window.open('https://www.christopherstock.de', '_blank');
-                    }
-
-                    if (
-                        Main.game.level.id === LevelId.LEVEL_DOJO
-                        && img === ImageData.BILLBOARD_WEBSITES
-                    ) {
-                        if (
-                            leftHalf
-                        ) {
-                            window.open('https://php8.christopherstock.de/architekt-baur/1.1/index.php/de/', '_blank');
-                        } else {
-                            window.open('https://www.winklerundschorn.de', '_blank');
-                        }
-                    }
-
-                    if (
-                        Main.game.level.id === LevelId.LEVEL_GARDEN
-                        && img === ImageData.BILLBOARD_GAMES
-                    ) {
-                        window.open('https://christopherstock.github.io/OutRunMF/dist/', '_blank');
-                    }
-
-                    if (
-                        Main.game.level.id === LevelId.LEVEL_TOWN
-                        && img === ImageData.BILLBOARD_3D_DEV
-                    ) {
-                        window.open('https://christopherstock.github.io/babylon-zero/dist/', '_blank');
-                    }
+                    window.open('https://php8.christopherstock.de/architekt-baur/1.1/index.php/de/', '_blank');
+                } else {
+                    window.open('https://www.winklerundschorn.de', '_blank');
                 }
             }
+
+            if (
+                Main.game.level.id === LevelId.LEVEL_GARDEN
+            ) {
+                window.open('https://christopherstock.github.io/OutRunMF/dist/', '_blank');
+            }
+
+            if (
+                Main.game.level.id === LevelId.LEVEL_TOWN
+            ) {
+                window.open('https://christopherstock.github.io/babylon-zero/dist/', '_blank');
+            }
         }
+    }
+
+    private onMove(event: PointerEvent): void {
+        if (Main.game.level === null) {
+            return;
+        }
+
+        // get the billboard Deco object for this level
+        const relevantBillboard: Decoration = this.getRelevantBillboard();
+        if (relevantBillboard === null) {
+            return;
+        }
+
+        // TODO redundant calc!
+        const handOverBillboard: boolean = (
+            event.clientX >= relevantBillboard.shape.body.bounds.min.x - Main.game.camera.getOffsetX()
+            && event.clientX < (relevantBillboard.shape.body.bounds.min.x + relevantBillboard.shape.getWidth() - Main.game.camera.getOffsetX())
+            && event.clientY >= relevantBillboard.shape.body.bounds.min.y - Main.game.camera.getOffsetY()
+            && event.clientY < (relevantBillboard.shape.body.bounds.min.y + relevantBillboard.shape.getHeight() - Main.game.camera.getOffsetY())
+        );
+
+        document.body.style.cursor = handOverBillboard ? 'pointer' : 'default';
+    }
+
+    private getRelevantBillboard(): Decoration {
+        const decos = Main.game.level.decosFg.concat(Main.game.level.decosBg);
+
+        for (const deco of decos) {
+            const img: string = deco.sprite.template.imageIds[0];
+            if (!img.includes('billboard')) {continue;}
+
+            if (
+                (Main.game.level.id === LevelId.LEVEL_START     && img === ImageData.BILLBOARD_WELCOME)
+                || (Main.game.level.id === LevelId.LEVEL_DOJO   && img === ImageData.BILLBOARD_WEBSITES)
+                || (Main.game.level.id === LevelId.LEVEL_GARDEN && img === ImageData.BILLBOARD_GAMES)
+                || (Main.game.level.id === LevelId.LEVEL_TOWN   && img === ImageData.BILLBOARD_3D_DEV)
+            ) {
+                return deco;
+            }
+        }
+
+        return null;
     }
 }
