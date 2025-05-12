5547b1e272ae82c0d0152ab480f4b10498e3976a
Christopher Stock <christopher.stock@mayflower.de>
2025-05-12 07:59:26 +0200

    revise start level

--- src/typescript/data/level/LevelStart.ts
+++ src/typescript/data/level/LevelStart.ts
@@ -33,46 +33,46 @@ export class LevelStart extends Level {
         // parallax bg "Mount Fuji"
         GameObjectFactory.createParallaxDeco(this, 0, 0, 1.0, DecoPosition.BG, SpriteTemplate.createFromSingleImage(ImageData.BG_MOUNT_FUJI));
 
-        this.addUpperGround();
+        this.addStartingZone();
         // this.addSlopeGround();
         // this.addLowerGround();
     }
 
-    private addUpperGround(): void {
-
-        const x = 0;
+    private addStartingZone(): void {
+        let x = 0;
 
         // site trigger with tree
         GameObjectFactory.createSiteTrigger(this, x + 0, 2000, 1000, 500, SiteContent.CONTENT_WELCOME, SitePanelAppearance.RIGHT, null);
         GameObjectBundleFactory.createDecoImage(this, x + 500, 2000, DecoPosition.FG, ImageData.TREE_1);
 
         // statue shrine
-        GameObjectBundleFactory.createStatusShrine(this, x + 1625, 2000);
-        /*
-        // destroyable crates
-        GameObjectBundleFactory.createObstacle(this, x + 2000, 2000, ImageData.CRATE_WOOD, Breakable.YES);
-        GameObjectBundleFactory.createObstacle(this, x + 2125, 2000, ImageData.CRATE_WOOD, Breakable.YES);
-        GameObjectBundleFactory.createObstacle(this, x + 2250, 2000, ImageData.CRATE_WOOD, Breakable.YES);
-        GameObjectBundleFactory.createObstacle(this, x + 2062, 1875, ImageData.CRATE_WOOD, Breakable.YES);
-        GameObjectBundleFactory.createObstacle(this, x + 2187, 1875, ImageData.CRATE_WOOD, Breakable.YES);
-*/
-        // billboard 'welcome'
-        GameObjectBundleFactory.createBillboard(this, x + 2500, 2000, DecoPosition.FG, ImageData.BILLBOARD_WELCOME);
+        GameObjectBundleFactory.createStatusShrine(this, x + 2750, 2000);
 
-        // grass
-        GameObjectBundleFactory.createDecoSprite(this, x + 2500, 2000, DecoPosition.FG, SpriteData.GRASS_1);
-        GameObjectBundleFactory.createDecoSprite(this, x + 2550, 2000, DecoPosition.FG, SpriteData.GRASS_1);
-        GameObjectBundleFactory.createDecoSprite(this, x + 2600, 2000, DecoPosition.FG, SpriteData.GRASS_1);
-        GameObjectBundleFactory.createDecoSprite(this, x + 3050, 2000, DecoPosition.FG, SpriteData.GRASS_2);
-        GameObjectBundleFactory.createDecoSprite(this, x + 3100, 2000, DecoPosition.FG, SpriteData.GRASS_2);
-        GameObjectBundleFactory.createDecoSprite(this, x + 3150, 2000, DecoPosition.FG, SpriteData.GRASS_2);
+        // billboard 'welcome' with grass
+        GameObjectBundleFactory.createBillboard(this, x + 3500, 2000, DecoPosition.FG, ImageData.BILLBOARD_WELCOME);
+        GameObjectBundleFactory.createDecoSprite(this, x + 3500, 2000, DecoPosition.FG, SpriteData.GRASS_1);
+        GameObjectBundleFactory.createDecoSprite(this, x + 3550, 2000, DecoPosition.FG, SpriteData.GRASS_1);
+        GameObjectBundleFactory.createDecoSprite(this, x + 3600, 2000, DecoPosition.FG, SpriteData.GRASS_1);
+        GameObjectBundleFactory.createDecoSprite(this, x + 4050, 2000, DecoPosition.FG, SpriteData.GRASS_2);
+        GameObjectBundleFactory.createDecoSprite(this, x + 4100, 2000, DecoPosition.FG, SpriteData.GRASS_2);
+        GameObjectBundleFactory.createDecoSprite(this, x + 4150, 2000, DecoPosition.FG, SpriteData.GRASS_2);
 
+        // tree, boulders & bushes
+        GameObjectBundleFactory.createDecoImage(this, x + 4830, 2000, DecoPosition.FG, ImageData.TREE_1, MirrorImage.YES);
+        GameObjectBundleFactory.createDecoImage(this, x + 5200, 2020, DecoPosition.BG, ImageData.BOULDER_1);
+        GameObjectBundleFactory.createDecoImage(this, x + 5300, 2000, DecoPosition.FG, ImageData.BOULDER_1);
+        GameObjectBundleFactory.createDecoImage(this, 6000 - 285, 2000, DecoPosition.FG, ImageData.BUSH_2);
+        GameObjectBundleFactory.createDecoImage(this, 6000 - 160, 2000 + 22, DecoPosition.BG, ImageData.BUSH_1);
+
+        // ground
+        GameObjectBundleFactory.createSolidGround(this, TilesetData.TILESET_SNOW, x + 0, 2000, 48,  5, Slope.NONE, CapHorz.NONE);
+/*
         // enemies
-        GameObjectBundleFactory.createEnemy(this, x + 3350, 2000, CharacterFacing.RIGHT, 3350, 4850, CharacterSpriteData.BLUE_NINJA_GUY, false);
-        GameObjectBundleFactory.createEnemy(this, x + 4850, 2000, CharacterFacing.LEFT, 3350, 4850, CharacterSpriteData.MASKED_NINJA_GUY, false);
+        GameObjectBundleFactory.createEnemy(this, x + 4350, 2000, CharacterFacing.RIGHT, 4350, 5850, CharacterSpriteData.BLUE_NINJA_GUY, false);
+        GameObjectBundleFactory.createEnemy(this, x + 5850, 2000, CharacterFacing.LEFT, 4350, 5850, CharacterSpriteData.MASKED_NINJA_GUY, false);
 
         // lion statue with trees and grass
-        GameObjectBundleFactory.createDecoImage(this, x + 3630, 2000, DecoPosition.FG, ImageData.TREE_1, MirrorImage.YES);
+        x = 1000;
         GameObjectBundleFactory.createDecoImage(this, x + 4265, 2000, DecoPosition.BG, ImageData.STATUE_LION);
         GameObjectBundleFactory.createDecoImage(this, x + 4580, 2000, DecoPosition.FG, ImageData.TREE_2);
         GameObjectBundleFactory.createDecoSprite(this, x + 4080, 2000, DecoPosition.FG, SpriteData.GRASS_2);
@@ -83,9 +83,16 @@ export class LevelStart extends Level {
         GameObjectBundleFactory.createDecoSprite(this, x + 4580, 2000, DecoPosition.FG, SpriteData.GRASS_1);
         GameObjectBundleFactory.createCandle(this, x + 4200, 2000, DecoPosition.FG);
         GameObjectBundleFactory.createCandle(this, x + 4540, 2000, DecoPosition.FG);
+*/
 
-        // upper ground
-        GameObjectBundleFactory.createSolidGround(this, TilesetData.TILESET_SNOW, x + 0,    2000, 40,  5, Slope.NONE, CapHorz.NONE);
+        /*
+        // destroyable crates
+        GameObjectBundleFactory.createObstacle(this, x + 2000, 2000, ImageData.CRATE_WOOD, Breakable.YES);
+        GameObjectBundleFactory.createObstacle(this, x + 2125, 2000, ImageData.CRATE_WOOD, Breakable.YES);
+        GameObjectBundleFactory.createObstacle(this, x + 2250, 2000, ImageData.CRATE_WOOD, Breakable.YES);
+        GameObjectBundleFactory.createObstacle(this, x + 2062, 1875, ImageData.CRATE_WOOD, Breakable.YES);
+        GameObjectBundleFactory.createObstacle(this, x + 2187, 1875, ImageData.CRATE_WOOD, Breakable.YES);
+*/
     }
 
     private addSlopeGround(): void {
